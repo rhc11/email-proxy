46b1321971e4644e1deea31ad957b2a30f8f24c3
Ruben Herrera <rubenherrera@wetaca.com>
2024-04-02 21:23:37 +0200

    FIX whatsappclient

--- src/services/whatsappClient.ts
+++ src/services/whatsappClient.ts
@@ -1,4 +1,5 @@
 import { Client, LocalAuth, Location } from "whatsapp-web.js"
+import qrcode from "qrcode-terminal"
 
 export const whatsappclient = new Client({
   authStrategy: new LocalAuth(),
@@ -8,3 +9,12 @@ export const location = new Location(
   Number(process.env.LATITUDE),
   Number(process.env.LONGITUDE)
 )
+
+whatsappclient.on("qr", async (qr) => {
+  qrcode.generate(qr, { small: true })
+})
+
+whatsappclient.on("disconnected", (reason) => {
+  console.log("Disconnected:", reason)
+  whatsappclient.initialize()
+})
